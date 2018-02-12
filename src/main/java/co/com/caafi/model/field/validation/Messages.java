b840aade967433c18c2468a9e433e39e46d823ec
Andres Castillo <acastillo119@gmail.com>
2018-02-12 07:42:55 -0500

    Ajustes en el formulario de datos generales del programa agregad el oattern validation

--- src/main/java/co/com/caafi/model/field/validation/Messages.java
+++ src/main/java/co/com/caafi/model/field/validation/Messages.java
@@ -3,6 +3,8 @@ package co.com.caafi.model.field.validation;
 public class Messages {
 	private String maxLength;
 	private String minLength;
+	private String pattern;
+	private String required;
 
 	public String getMaxLength() {
 		return maxLength;
@@ -18,6 +20,22 @@ public class Messages {
 
 	public void setMinLength(String minLength) {
 		this.minLength = minLength;
+	}
+
+	public String getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+
+	public String getRequired() {
+		return required;
+	}
+
+	public void setRequired(String required) {
+		this.required = required;
 	}
 	
 	
