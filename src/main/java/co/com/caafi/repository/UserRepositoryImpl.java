e6fc381118b8ac9238087f8dfc237582d0f4e97f
Carlos Castro <castroscarlos1@gmail.com>
2018-05-14 18:14:57 -0500

    Se ajusta loggin

--- src/main/java/co/com/caafi/repository/UserRepositoryImpl.java
+++ src/main/java/co/com/caafi/repository/UserRepositoryImpl.java
@@ -16,58 +16,63 @@ import co.edu.udea.wsClient.OrgSistemasWebServiceClient;
 
 @Repository
 @PropertySource("classpath:udea.properties")
-@Profile({"pdn","lab"})
+@Profile({ "pdn", "lab" })
 public class UserRepositoryImpl implements UserRepository {
 
-	@Value("${TOKEN_PDN}")
-	String token;
-	@Value("${CLAVE_PUBLICA}")
-	String publicKey;
-	@Value("${SERVICIO_VALIDAR_USUARIO}")
-	String serviceName;
-	@Value("${SERVICIO_SIPE}")
-	String serviceNameSipe;
-	@Value("${SIPE_PARM_CC}")
-	String paramSipeCC;
+    private static final String STUDENT = "STUDENT";
+    private static final String ADMIN = "ADMIN";
+    private static final String CLAVE = "clave";
+    private static final String USUARIO = "usuario";
+    @Value("${TOKEN_PDN}")
+    String token;
+    @Value("${CLAVE_PUBLICA}")
+    String publicKey;
+    @Value("${SERVICIO_VALIDAR_USUARIO}")
+    String serviceName;
+    @Value("${SERVICIO_SIPE}")
 
-	public User getUser(String name, String password) {
-		String doc;
-		User user = null;
-		OrgSistemasWebServiceClient wsClient;
-		try {
+    String serviceNameSipe;
+    @Value("${SIPE_PARM_CC}")
+    String paramSipeCC;
 
-			wsClient = new OrgSistemasWebServiceClient(publicKey);
-			wsClient.addParam("usuario", name);
-			wsClient.addParam("clave", password);
-			doc = wsClient.obtenerString(serviceName, token).trim();
-		} catch (OrgSistemasSecurityException | Exception ex) {
-			return null;
-		}
-		if (doc == null || "".equals(doc)) {
-			return null;
-		}
-		user = new User();
-		user.setPass(password);
-		user.setName(name);
-		user.setUserName(name);
-		user.setDocument(doc);
-		user.setRole(new ArrayList<String>(Arrays.asList("STUDENT")));
-		try {
-			List<SIPEEmployee> employeeList;
-			wsClient = new OrgSistemasWebServiceClient();
-			wsClient.addParam(paramSipeCC, doc);
-			employeeList = wsClient.obtenerBean(serviceNameSipe, token, SIPEEmployee.class);
-			int lastRecordIndex = employeeList.size() - 1;
-			if (!employeeList.isEmpty() && employeeList.get(lastRecordIndex) != null) {
-				SIPEEmployee employee = employeeList.get(lastRecordIndex);
-				user.setRole(new ArrayList<String>(Arrays.asList("ADMIN")));
-				user.setName(employee.getNombre());
-				return user;
-			}
-		} catch (OrgSistemasSecurityException | Exception e) {
-		}
-		return user;
+    public User getUser(String name, String password) {
+        String doc;
+        User user = null;
+        OrgSistemasWebServiceClient wsClient;
+        try {
 
-	}
+            wsClient = new OrgSistemasWebServiceClient(publicKey);
+            wsClient.addParam(USUARIO, name);
+            wsClient.addParam(CLAVE, password);
+            doc = wsClient.obtenerString(serviceName, token).trim();
+        } catch (OrgSistemasSecurityException | Exception ex) {
+            return null;
+        }
+        if (doc == null || "".equals(doc.trim())) {
+            return null;
+        }
+        user = new User();
+        user.setPass(password);
+        user.setName(name);
+        user.setUserName(name);
+        user.setDocument(doc);
+        user.setRole(new ArrayList<String>(Arrays.asList(STUDENT)));
+        try {
+            List<SIPEEmployee> employeeList;
+            wsClient = new OrgSistemasWebServiceClient();
+            wsClient.addParam(paramSipeCC, doc);
+            employeeList = wsClient.obtenerBean(serviceNameSipe, token, SIPEEmployee.class);
+            int lastRecordIndex = employeeList.size() - 1;
+            if (!employeeList.isEmpty() && employeeList.get(lastRecordIndex) != null) {
+                SIPEEmployee employee = employeeList.get(lastRecordIndex);
+                user.setRole(new ArrayList<String>(Arrays.asList(ADMIN)));
+                user.setName(employee.getNombre());
+                return user;
+            }
+        } catch (OrgSistemasSecurityException | Exception e) {
+        }
+        return user;
+
+    }
 
 }
