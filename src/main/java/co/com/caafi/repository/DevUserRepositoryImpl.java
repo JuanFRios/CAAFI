b2638734e7f44b8b9218f5f33e3253cde186590d
Andres Castillo <acastillo119@gmail.com>
2018-05-24 06:06:05 -0500

    configuración

--- src/main/java/co/com/caafi/repository/DevUserRepositoryImpl.java
+++ src/main/java/co/com/caafi/repository/DevUserRepositoryImpl.java
@@ -11,7 +11,7 @@ import co.com.caafi.model.User;
 
 @Repository
 @PropertySource("classpath:udea.properties")
-@Profile("local")
+@Profile("dev")
 public class DevUserRepositoryImpl implements UserRepository {
 
 	public User getUser(String name, String password) {
