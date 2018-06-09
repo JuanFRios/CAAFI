8d86c890f3864a71f42d1ee74313b16729feea75
Andres Castillo <acastillo119@gmail.com>
2018-06-09 17:50:28 -0500

    ajuste paquete

--- src/main/java/co/com/caafi/rest/DataResource.java
+++ src/main/java/co/com/caafi/rest/DataResource.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import co.com.caafi.model.User;
 import co.com.caafi.model.template.FormData;
