2653756164d7848d9f68ef41c69fe60687567a9d
Andrés Felipe Castillo Escobar <acastillo119@gmail.com>
2018-07-24 15:29:35 -0500

    envío de correos

--- src/main/java/co/com/caafi/service/EmailService.java
+++ src/main/java/co/com/caafi/service/EmailService.java
@@ -1,8 +1,11 @@
 package co.com.caafi.service;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +15,24 @@ public class EmailService {
 	public JavaMailSender emailSender;
 	
 	public void sendEmail(String to, String subject, String text) {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	     
+	    mailSender.setUsername("caafi@udea.edu.co");
+	    mailSender.setPassword("udea.2014@");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+		
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
 		message.setSubject(subject);
 		message.setText(text);
-		emailSender.send(message);
+		mailSender.send(message);
 	}
 
 }
