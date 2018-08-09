f612d1306022c570d227a60b36bef3fd59c115fb
Andres Castillo <acastillo119@gmail.com>
2018-08-09 05:27:54 -0500

    ajuste email sender en el boot de la aplicación

--- src/main/java/co/com/caafi/CaafiApplication.java
+++ src/main/java/co/com/caafi/CaafiApplication.java
@@ -2,11 +2,18 @@ package co.com.caafi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CaafiApplication {
+public class CaafiApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CaafiApplication.class);
+    }
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CaafiApplication.class, args);
 	}
 }
