82ad7d6e9a7583f71fc4d84c33327cc9a18b5a37
Andres Castillo <acastillo119@gmail.com>
2018-08-08 23:07:57 -0500

    ajuste email service

--- src/main/java/co/com/caafi/service/EmailService.java
+++ src/main/java/co/com/caafi/service/EmailService.java
@@ -1,11 +1,8 @@
 package co.com.caafi.service;
 
-import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,24 +12,11 @@ public class EmailService {
 	public JavaMailSender emailSender;
 	
 	public void sendEmail(String to, String subject, String text) {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("caafi@udea.edu.co");
-	    mailSender.setPassword("udea.2014@");
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-		
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
 		message.setSubject(subject);
 		message.setText(text);
-		mailSender.send(message);
+		emailSender.send(message);
 	}
 
 }
