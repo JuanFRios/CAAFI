eda4400d22a6a9d3f981394f74ac4888607bde04
Andres Castillo <acastillo119@gmail.com>
2018-08-09 06:31:16 -0500

    debug mode

--- src/main/java/co/com/caafi/CaafiApplication.java
+++ src/main/java/co/com/caafi/CaafiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaafiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CaafiApplication.class, args);
+		SpringApplication.run(CaafiApplication.class, "--debug");
 	}
 }
