0bce16aa50506efca2237406d640d4dbcaeaf614
Andrés Felipe Castillo Escobar <acastillo119@gmail.com>
2019-07-19 14:09:33 -0500

    ajuste tabla

--- frontend/caafi/src/app/services/data.service.ts
+++ frontend/caafi/src/app/services/data.service.ts
@@ -104,7 +104,7 @@ export class DataService {
                   '" download><span style="font-size: 20px; color: #0a351c">' +
                   '<i class="fas fa-download"></i></span></a>, ';
               } else {
-                if (typeof data[i][j][k] === 'object') {
+                if (typeof data[i][j][k] === 'object' && data[i][j][k] != null) {
                   dataRepeat += namesRepeats[k] + ': ' + data[i][j][k].toString() + ', ';
                 } else {
                   dataRepeat += namesRepeats[k] + ': ' + data[i][j][k] + ', ';
@@ -151,7 +151,7 @@ export class DataService {
           for (let j = 0; j < data[i].length; j++) {
             dataRepeat += '{ ';
             for (const k in data[i][j]) {
-              if (typeof data[i][j][k] === 'object') {
+              if (typeof data[i][j][k] === 'object' && data[i][j][k] != null) {
                 dataRepeat += namesRepeats[k] + ': ' + data[i][j][k].toString() + ', ';
               } else {
                 dataRepeat += namesRepeats[k] + ': ' + data[i][j][k] + ', ';
