3f76dddd6c53cadfa747711df9b2f9d0eee209af
Andres Castillo <acastillo119@gmail.com>
2020-07-14 09:40:44 -0500

    ajuste error envio de correos de encuestas

--- src/main/java/co/com/caafi/config/MongoDBConfiguration.java
+++ src/main/java/co/com/caafi/config/MongoDBConfiguration.java
@@ -1,6 +1,12 @@
 package co.com.caafi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
@@ -11,4 +17,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration
 public class MongoDBConfiguration {
 
+  @Bean
+  public MappingMongoConverter mongoConverter(MongoDbFactory mongoFactory, MongoMappingContext mongoMappingContext) {
+    DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoFactory);
+    MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+    mongoConverter.setMapKeyDotReplacement(".");
+
+    return mongoConverter;
+  }
 }
