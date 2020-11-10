6501f666ef1ed523813af64f544396dfbfd812c4
Jorge <desarrolloingenieria9@udea.edu.co>
2020-11-10 17:36:45 -0500

    Front HU-18: se agrega boton para reiniciar formularios

--- frontend/caafi/src/app/form/modal-form/modal-form.component.ts
+++ frontend/caafi/src/app/form/modal-form/modal-form.component.ts
@@ -1,6 +1,7 @@
 import { OnInit, Component } from '@angular/core';
 import { FormComponent } from '../form.component';
 import { FormService } from 'src/app/service/form.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   template: `EMPTY`
@@ -15,6 +16,10 @@ export abstract class ModalFormComponent extends FormComponent implements OnInit
 
   ngOnInit(): void {
     super.ngOnInit();
+  }
+
+  cleanForm(): void {
+    this.form.reset();
   }
 
 }
