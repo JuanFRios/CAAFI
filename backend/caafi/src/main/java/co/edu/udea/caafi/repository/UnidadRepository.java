1a7b9a4fe24c9baa3c7d51a3cc0ae6f3e6d4ed82
Juan Fernando Rios <juan.rios27@udea.edu.co>
2021-09-03 09:52:33 -0500

    Desarticulación con el backend

--- backend/caafi/src/main/java/co/edu/udea/caafi/repository/UnidadRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udea.caafi.repository;
-
-import co.edu.udea.caafi.model.facultad.Unidad;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UnidadRepository extends MongoRepository<Unidad, String> {
-}
--- backend/caafi/src/main/java/co/edu/udea/caafi/service/ListService.java
+++
@@ -1,7 +0,0 @@
-package co.edu.udea.caafi.service;
-
-import co.edu.udea.caafi.dto.resource.form.list.ListDto;
-import co.edu.udea.caafi.model.resource.form.list.List;
-
-public interface ListService extends ResourceService<List, ListDto> {
-}
